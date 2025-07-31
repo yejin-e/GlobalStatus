e19a854be61e9dce0ef2f81e86fcca4ea4ed27c5
Yejin <yejin01e@gmail.com>
2025-07-31 20:57:04 +0900

    feat
    더미데이터 수정

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { PostContext } from "../contexts/post";
 import { Link, useNavigate } from "react-router-dom";
+import Create from "../icons/Create";
 
 const MainPage = () => {
   const navigate = useNavigate();
@@ -8,21 +9,21 @@ const MainPage = () => {
   console.log(posts);
 
   return (
-    <div className="w-screen h-screen">
-      <div className="p-10 text-center text-5xl">게시판</div>
-      <Link className="p-1 border w-fit h-fit place-content-end" to={"/create"}>
-        go Create
+    <div>
+      <Link className="fixed bottom-5 right-5" to={"/create"}>
+        <Create />
       </Link>
+      <div className="p-10 text-center text-5xl">게시판</div>
       {posts.map((post, index) => (
         <div
-          className="border w-9/10 m-auto p-4"
+          className="border border-gray-300 w-9/10 m-auto p-5"
           key={index}
           onClick={() => {
             navigate(`/detail/${index}`);
           }}
         >
           <div className="font-bold">{post.title}</div>
-          <div>{post.content}</div>
+          <div className="truncate">{post.content}</div>
         </div>
       ))}
     </div>
